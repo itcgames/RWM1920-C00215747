a7abb5afa05509e164e0ddcd4ce371310d4f01f4
SavannahBolger <C00215747@itcarlow.ie>
2019-11-12 10:49:40 +0000

    component function edit

---
+++ Component/Assets/SpringFunction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringFunction : MonoBehaviour
+{
+    public GameObject gameObject;
+    
+    private float jump = 44.27f;
+
+    void Update()
+    {
+        gameObject.velocityX = gameObject.velocityX + jump;
+    }
+}
---
+++ Component/Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class move : MonoBehaviour
+{
+    float velocityX = 0;
+    float velocityY = 0;
+
+    void Update()
+    {
+        float time = 1.0f / 60.0f;
+        Position.X = Position.X + (velocityX * time) + 0.5f * 9.8f * (time * time);
+        Position.Y = Position.Y + (velocityX * time) + 0.5f * 0.0f * (time * time);
+        velocityX = velocityX + 9.8f * time;
+        velocityY = velocityY + 0.0f * time;
+    }
+}
