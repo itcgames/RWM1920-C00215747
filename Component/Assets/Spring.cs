14fb515fae591cebddadee75e501e1c06e0f33f3
SavannahBolger <C00215747@itcarlow.ie>
2019-11-25 08:04:52 +0000

    animate

---
+++ Component/Assets/Spring.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Spring : MonoBehaviour
+{
+
+    public LayerMask layerMask;
+    public Transform rayCastStart;
+    public Transform rayCastEnd;
+    public float springForce = 1200.0f;
+
+    private Animator animator;
+    private float rayCastDistance;
+    private GameObject player;
+
+    private bool SpringInputActive
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Space);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (rayCastStart != null && rayCastEnd != null)
+        {
+            rayCastDistance = rayCastEnd.position.x - rayCastStart.position.x;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(rayCastStart.position, rayCastEnd.position, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(rayCastStart.position, Vector2.right, rayCastDistance, layerMask);
+        if (hit.collider != null && !animator.GetBool("Pressing"))
+        {
+            animator.SetBool("Pressing", true);
+            animator.SetBool("Releasing", false);
+            player = hit.collider.gameObject;
+        }
+        else if (hit.collider != null /*&& animator.GetBool("Pressing")*/ && SpringInputActive)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, springForce));
+        }
+        else if (hit.collider == null)
+        {
+            animator.SetBool("Pressing", false);
+            animator.SetBool("Releasing", true);
+        }
+    }
+}
--- Component/Assets/move.cs
+++ Component/Assets/move.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class move : MonoBehaviour
 {
-    float velocityX = 0;
-    float velocityY = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        float time = 1.0f / 60.0f;
-        Position.X = Position.X + (velocityX * time) + 0.5f * 9.8f * (time * time);
-        Position.Y = Position.Y + (velocityX * time) + 0.5f * 0.0f * (time * time);
-        velocityX = velocityX + 9.8f * time;
-        velocityY = velocityY + 0.0f * time;
+        
     }
 }
